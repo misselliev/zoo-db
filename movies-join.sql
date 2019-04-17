bed8e04fa910b8fd908e3d0655b5b641e042b802
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-17 16:37:00 -0700

    07 More join

--- movies-join.sql
+++ movies-join.sql
@@ -1,4 +1,6 @@
-/*1. List the films where the yr is 1962 [Show id, title]-*/
+/* 07 More JOIN
+
+1. List the films where the yr is 1962 [Show id, title]-*/
 SELECT id, title FROM movie WHERE yr=1962;
 
  /*2. Give year of 'Citizen Kane'.*/
